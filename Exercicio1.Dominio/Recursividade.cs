aed46b3bdbb36995e15d84b52634753cd02969ba
alexssantiago <alexssantiago09@gmail.com>
2019-02-28 10:47:10 -0300

    Add project files.

---
+++ Exercicio1.Dominio/Recursividade.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+
+namespace Exercicio1.Dominio
+{
+    public static class Recursividade
+    {
+        public static int Fatorial(int n)
+        {
+            if (n == 0)
+                return 1;
+
+            int fat = n;
+
+            for (int x = (n - 1); x > 0; x--)
+            {
+                fat *= x;
+            }
+
+            return fat;
+        }
+
+        public static int FatorialRecursivo(int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * FatorialRecursivo(n - 1);
+        }
+
+        public static int Fibonacci(int n)
+        {
+            //Fn = Fn-1 + Fn-2
+
+            int Fn = 0;
+            int[] F = new int[n + 1];
+
+            //Por definição, temos que:
+            F[0] = F[1] = 1;
+
+            //Calculando F(2) em diante...
+            for (int i = 2; i < F.Length; i++)
+            {
+                F[i] = F[i - 1] + F[i - 2];
+                Fn = F[i];
+            }
+
+            return Fn;
+        }
+
+        public static int FibonacciRecursivo(int n)
+        {
+            if (n < 2)
+                return 1;
+            else
+                return FibonacciRecursivo(n - 1) + FibonacciRecursivo(n - 2);
+        }
+
+        public static string TiraVogal(string frase)
+        {
+            string fraseFinal = string.Empty;
+
+            var vogais = new List<string> { "a", "e", "e", "i", "o", "u" };
+
+            foreach (char letra in frase)
+            {
+                if (!vogais.Contains(letra.ToString().ToLower()))
+                    fraseFinal += letra;
+            }
+
+            return fraseFinal;
+        }
+
+        public static string TiraVogalRecursivo(string frase)
+        {
+            var letra = frase[frase.Length - 1];
+            var vogais = new List<char> { 'a', 'e', 'e', 'i', 'o', 'u' };
+
+            if (frase.Equals(""))
+                return string.Empty;
+            else if (vogais.Contains(letra))
+                return TiraVogalRecursivo(frase.Trim(letra));
+            else
+                return "";
+            //return TiraVogalRecursivo(frase.TrimEnd(letra));
+
+        }
+
+        public static int SomaDiagonais(int[,] mat)
+        {
+            int soma = 0, j = mat.GetLength(1) - 1;
+
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                soma += mat[i, i] + mat[i, j];
+                j--;
+            }
+            
+            return soma;
+        }
+
+        public static int SomaDiagonaisRecursivo(int[,] mat, int n)
+        {
+            if (n < 2)
+                return mat[0, 0] + mat[0, mat.GetLength(1) - 1];
+            else
+                return mat[n - 1, n - 1] + mat[n - 1, mat.GetLength(1) - n] + SomaDiagonaisRecursivo(mat, n - 1);
+        }
+
+    }
+}
---
+++ Exercicio1/Program.cs
@@ -0,0 +1,35 @@
+﻿using Exercicio1.Dominio;
+using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Recursividade.FatorialRecursivo(4));
+            //Console.WriteLine(Recursividade.FibonacciRecursivo(6));
+            //Console.WriteLine(Recursividade.TiraVogalRecursivo("Algoritmos e Estruturas de dados")); // lgrtms strtrs d dds
+
+            int[,] mat = new int[3, 3];
+
+            mat[0, 0] = 7; mat[0, 1] = 10; mat[0, 2] = 4;
+            mat[1, 0] = 12; mat[1, 1] = 5; mat[1, 2] = 9;
+            mat[2, 0] = 2; mat[2, 1] = 8; mat[2, 2] = 11;
+
+            //mat[0, 0] = 11; mat[0, 1] = 8; mat[0, 2] = 6; mat[0, 3] = 6;
+            //mat[1, 0] = 0; mat[1, 1] = 2; mat[1, 2] = 17; mat[1, 3] = 1;
+            //mat[2, 0] = 7; mat[2, 1] = 3; mat[2, 2] = 10; mat[2, 3] = -1;
+            //mat[3, 0] = 9; mat[3, 1] = 5; mat[3, 2] = 12; mat[3, 3] = 7;
+
+            //mat[0, 0] = 7; mat[0, 1] = 10;
+            //mat[1, 0] = 4; mat[1, 1] = 14;
+
+            Console.WriteLine(Recursividade.SomaDiagonais(mat));
+            Console.WriteLine(Recursividade.SomaDiagonaisRecursivo(mat, 3));
+
+            Console.ReadKey();
+
+        }
+    }
+}
